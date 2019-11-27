aca555c9010321aeabcc350731f6916f1ec8c1f9
Eric Ebulu <ericebulu@Erics-MacBook-Pro.local>
2019-11-27 15:03:25 +0200

    ft(structure): rename files - change folder structure - add url test - rename file and folder
    [Delivers #167353304]

--- e2e/app.test.js
+++ e2e/app.test.js
@@ -1,9 +1,14 @@
 import { Selector } from 'testcafe';
 
 //a test for the https://ekumamait.github.io/Profile/UI/ 
+const origin = "https://ekumamait.github.io/Profile/UI/";
 
-fixture `Getting Started`
-    .page `https://ekumamait.github.io/Profile/UI/`;
+fixture `Profile page tests`
+    .page `${origin}`;
+
+test('Test page URL', async t =>{
+    await t.expect(origin).match(/https:\/\/ekumamait.github.io\/Profile\/UI/);
+}) 
 
 test('Profile-headline', async t =>{
     await t
